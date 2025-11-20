86ed5e7b59cd1c7094f341b20431701931c5e1f9
Andycacera <andy.cacera@gmail.com>
2025-11-19 23:17:12 -0600

    fix: 🐛 testing_fixes
    assuring the correct execution of the test command

--- src/modules/users/users.service.spec.ts
+++ src/modules/users/users.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { JsonDbService } from '../../common/json-db/json-db.service';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersService, JsonDbService],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
